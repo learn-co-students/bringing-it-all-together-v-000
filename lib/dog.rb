410cf61bb937e1197065f642fa8d7e754d42831c
NStephenson <nickstephenson90@gmail.com>
2016-02-24 01:14:14 -0500

    made all tests pass

---
+++ lib/dog.rb
@@ -0,0 +1,89 @@
+require 'pry'
+
+class Dog
+
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      );
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs;")
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs(name, breed)
+      VALUES (?,?);
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+
+  def self.create(row)
+    dog = self.new(row)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+    SQL
+    data = DB[:conn].execute(sql, id).first
+    self.new(id: data[0], name: data[1], breed: data[2])
+  end
+
+  def self.find_or_create_by(id)
+    sql = "SELECT * FROM dogs WHERE name =? AND breed =?"
+    dog = DB[:conn].execute(sql, id[:name], id[:breed])
+    if !dog.empty?
+      dog_data = dog.first
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(id)
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ?
+      WHERE id = ?;
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+
+end
