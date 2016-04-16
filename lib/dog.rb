e29a9894d0453a1f49f3e3c94f231a3f3cf26c9b
aprietof <adrianprieto00@gmail.com>
2016-04-16 15:36:31 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,6 +37,54 @@ class Dog
     SQL
 
     DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() from dogs")[0][0]
+  end
+
+  # takes in a hash of attributes and uses metaprogramming
+  # to create a new dog object. Then it uses the #save
+  # method to save that dog to the database
+  def self.create(name: , breed:)
+    dog = self.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+
+  # creates an instance with corresponding attribute values
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+
+  # returns a new dog object by id
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ?
+    SQL
+
+    row = DB[:conn].execute(sql, id).flatten
+    self.new_from_db(row)
+  end
+
+  # returns an instance of dog that matches the name from the DB
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+    SQL
+
+    row = DB[:conn].execute(sql, name).flatten
+    self.new_from_db(row)
+  end
+
+  # creates an instance of a dog if it does not already exist
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: name)
+    end
+    dog
   end
 
 end
