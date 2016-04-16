8da6a5bb8640e90f0384ac40a12e9f3111e0be89
aprietof <adrianprieto00@gmail.com>
2016-04-16 14:24:49 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog
+
+  # has a name and a breed
+  attr_accessor :id, :name, :breed
+
+  # has an id that defaults to `nil` on initialization
+  # accepts key value pairs as arguments to initialize
+  def initialize(id: nil, name:, breed:)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  # creates the dogs table in the database
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      );
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  # drops the dogs table from the database
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+  end
+
+  # saves an instance of the dog class to the database
+  # and then sets the given dogs `id` attribute
+  def save
+
+  end
+
+end
