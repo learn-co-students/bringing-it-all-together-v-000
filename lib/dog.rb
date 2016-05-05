0046ff2799d85911e1ca7048bcfa9a14295afda3
krisnotchris <krishenderson@gmail.com>
2016-05-04 19:22:20 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,6 +26,11 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
   def save
     if self.id
       self.update
@@ -63,11 +68,39 @@ class Dog
       name = dog_data[1]
       breed = dog_data[2]
       Dog.new(id: id, name: name, breed: breed)
-      #binding.pry
     else
-      new_dog = self.create(name: name, breed: breed)
-      new_dog.save
+      dog = self.create(name: name, breed: breed)
+      dog.save
+      dog
     end
+  end
+
+  def self.new_from_db(row)
+    #binding.pry
+    
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    dog = Dog.new(id: id,name: name, breed: breed)
+    dog
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ?
+    SQL
+    result = DB[:conn].execute(sql, name)[0]
+    id = result[0]
+    name = result[1]
+    breed = result[2]
+    Dog.new(id: id, name: name, breed: breed)
+  end
+
+  def update
+    sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
