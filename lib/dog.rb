684ad6f1a745d7c54e688a4bb2bcf923ed738f30
motti770 <mottigottlieb@gmail.com>
2016-05-09 17:21:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,28 +21,62 @@ class Dog
   def save
     sql = 'INSERT INTO dogs (name,breed) VALUES (?,?)'
     DB[:conn].execute(sql,self.name,self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+
   end
- 
-  def self.create(name:,breed:)
-    new_dog = Dog.new(name, breed)
+
+  def self.create(id=nil,name:,breed:)
+    new_dog = Dog.new(name: name, breed: breed)
     new_dog.save
+    new_dog
   end
 
-  #binding.pry
+    def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
 
+    def self.find_by_name(name)
+       sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+       DB[:conn].execute(sql,name).map do |row|
+        self.new_from_db(row)
+       end.first
+    end
 
-#   describe "#create" do
-#     it 'takes in a hash of attributes and uses metaprogramming to create a new dog object. Then it uses the #save method to save that dog to the database'do
-#       Dog.create(name: "Ralph", breed: "lab")
-#       expect(DB[:conn].execute("SELECT * FROM dogs")).to eq([[1, "Ralph", "lab"]])
-#     end
-#     it 'returns a new dog object' do
-#       dog = Dog.create(name: "Dave", breed: "podle")
+   def update
+    sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
-#       expect(teddy).to be_an_instance_of(Dog)
-#       expect(dog.name).to eq("Dave")
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+
+ DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+#     describe '.find_by_id' do
+#     it 'returns a new dog object by id' do
+#       dog = Dog.create(name: "Kevin", breed: "shepard")
+
+#       dog_from_db = Dog.find_by_id(1)
+
+#       expect(dog_from_db.id).to eq(1)
 #     end
 #   end
-
 
 end
