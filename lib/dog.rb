6463b7a2a22ca24b27e98186c238fc548b8230d4
Michael Chen <michael.chen.1120@gmail.com>
2016-05-13 13:55:24 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,89 @@
+class Dog
+  attr_reader :id
+  attr_accessor :name, :breed
+
+  def initialize(id:nil, name:, breed:)
+    @id=id
+    @name=name
+    @breed=breed
+  end
+
+  def self.create_table
+    sql=<<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    );
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql=<<-SQL
+    DROP TABLE IF EXISTS dogs
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql=<<-SQL
+    INSERT INTO dogs (name,breed)
+    VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql,self.name,self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+
+  def self.create(name:,breed:)
+    new_dog=self.new(name: name, breed: breed)
+    new_dog.save
+    new_dog
+  end
+
+  def self.find_by_id(id)
+    sql=<<-SQL
+    SELECT * FROM dogs WHERE id = ?
+    SQL
+
+    result=DB[:conn].execute(sql,id)[0]
+    self.new(id: result[0],name: result[1],breed: result[2])
+  end
+
+  def self.find_or_create_by(name:,breed:)
+    dog=DB[:conn].execute("SELECT * FROM dogs WHERE name= ? AND breed= ?",name,breed)
+    if !dog.empty?
+      dog_data=dog[0]
+      dog=Dog.new(id: dog_data[0],name: dog_data[1],breed: dog_data[2])
+    else
+      dog=self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
+    new_dog=self.new(id: row[0], name: row[1], breed: row[2])
+    new_dog
+  end
+
+  def self.find_by_name(name)
+    sql=<<-SQL
+    SELECT * FROM dogs WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql=<<-SQL
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql,self.name,self.breed,self.id)
+  end
+end
