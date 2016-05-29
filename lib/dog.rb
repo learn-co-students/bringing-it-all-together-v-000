342d6b9cf821b4ca89a6bce50e9da94cd8de974f
Michael Randell <michael.james.randell@gmail.com>
2016-05-29 12:47:57 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,25 +28,84 @@ class Dog
 
     DB[:conn].execute(sql)
   end
-=begin
+
   def self.new_from_db(row)
-    dog_creation = self.new(row[0], row[1], row[2])
+    dog_creation = Dog.new(name: row[1], breed: row[2], id: row[0])
     dog_creation
   end
-=end
-  def save
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed) VALUES (?, ?)
-    SQL
 
-    DB[:conn].execute(sql, self.name, self.breed)
-    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    self
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) VALUES (?, ?)
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.breed)
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+    end
   end
 
   def self.create(name:, breed:)
     new_dog = Dog.new(name: name, breed: breed)
     new_dog.save
     new_dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, id).map do |x|
+      self.new_from_db(x)
+    end.first
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |x|
+      self.new_from_db(x)
+    end.first
+  end
+
+  def self.find_by_breed(breed)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE breed = ?
+    SQL
+
+    DB[:conn].execute(sql, breed).map do |x|
+      self.new_from_db(x)
+    end.first
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    if self.find_by_breed(breed) && self.find_by_name(name)
+      self.find_by_breed(breed)
+      self.find_by_name(name)
+    else
+      self.create(name: name, breed: breed)
+    end
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
