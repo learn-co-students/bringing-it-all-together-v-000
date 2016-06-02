5236ba3c2f0f2d72fa6227a59bd853fe3f105aca
Shmuwol <shmullywolfson@gmail.com>
2016-06-02 00:27:38 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment.rb"
 class Dog
   attr_accessor :name, :breed, :id
 
-  def initialize(name:, breed:, id: nil)
+  def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
@@ -41,7 +41,7 @@ class Dog
 
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-
+      self
     end
   end
 
@@ -55,10 +55,41 @@ class Dog
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
-    self.new(id, name, breed)
+    self.find_or_create_by(name: row[1], breed: row[2])
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    self.new(id: result[0], name: result[1], breed: result[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ? AND breed = ?
+    SQL
+
+    dog = DB[:conn].execute(sql, name, breed)
+    dog_data = dog[0]
+    if !dog.empty?
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name:name, breed: breed)
+    end
+    dog
   end
 
 end
