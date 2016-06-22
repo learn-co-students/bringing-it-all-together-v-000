fc093f2f688a5c170358e6647bbf52b8c2f07085
Andrew Papacostas <a_papacostas@yahoo.com>
2016-06-22 06:25:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,60 @@
+require_relative "../config/environment.rb"
+class Dog
+  attr_accessor :id, :name, :breed
+
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-sql
+    CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT)
+    sql
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-sql
+    DROP TABLE dogs
+    sql
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+
+  def self.create(name:, breed:)
+    dogs = Dog.new(name: name, breed: breed)
+    dogs.save
+    dogs
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT id, name, breed FROM dogs WHERE id = ?"
+    row = DB[:conn].execute(sql,id)[0]
+    Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+
+
+
+
+  def self.new_from_db(row)
+    new_dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+    new_dog
+  end
+
+end
