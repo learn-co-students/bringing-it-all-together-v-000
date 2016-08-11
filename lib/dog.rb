3d24b15b4b8aed50c858a557d1e4cbbcb3e85284
ThisIsTyDell <milleb94@gmail.com>
2016-08-11 16:58:05 -0500

    added method to create new object from database

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,6 +44,13 @@ class Dog
     new_dog
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
