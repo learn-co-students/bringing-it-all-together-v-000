af4fa7bd726043da6c09f88d0efa28b6210349d8
hilaryml <hilyttle@gmail.com>
2016-08-11 15:07:21 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,80 @@
+class Dog
+
+	attr_accessor :name, :breed
+	attr_reader :id
+
+	def initialize(id: nil, name:, breed:)
+		@id = id
+		@name = name
+		@breed = breed
+	end
+
+	def self.create_table
+		DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+		sql = <<-sql
+			CREATE TABLE dogs (
+				id INTEGER PRIMARY KEY,
+				name TEXT,
+				breed TEXT)
+		sql
+
+		DB[:conn].execute(sql)
+	end
+
+	def self.drop_table
+		sql = "DROP TABLE dogs"
+
+		DB[:conn].execute(sql)
+	end
+
+	def self.new_from_db(row)
+		dog = self.new(id: row[0], name: row[1], breed: row[2])
+		dog
+	end
+
+	def self.find_by_name(name)
+		sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+
+		DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
+	end
+
+	def update
+		sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+
+		DB[:conn].execute(sql, self.name, self.breed, self.id)
+	end
+
+	def save
+		if self.id
+			self.update
+		else
+			sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+
+			DB[:conn].execute(sql, self.name, self.breed)
+
+			@id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+		end
+		self
+	end
+
+	def self.create(name:, breed:)
+		dog = Dog.new(name: name, breed: breed)
+		dog.save
+	end
+
+	def self.find_by_id(id)
+		sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1"
+
+		DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}.first
+	end
+
+	def self.find_or_create_by(name:, breed:)
+		sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
+		if DB[:conn].execute(sql, name, breed).empty?
+			self.create(name: name, breed: breed)
+		else
+			DB[:conn].execute(sql, name, breed).map {|row| self.new_from_db(row)}.first
+		end
+	end
+
+end
