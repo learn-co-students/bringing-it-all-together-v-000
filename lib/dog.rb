d688d26df1d63ced985bff8c9f75eb88c37ec7e8
ThisIsTyDell <milleb94@gmail.com>
2016-08-11 16:10:39 -0500

    add method to create table in SQL if one does not exist for dogs

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,5 +7,15 @@ class Dog
     @name = name
     @breed = breed
   end
-  
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        );
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
