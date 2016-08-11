35a066dfdfe355338648d03f12947a6f459d56be
ThisIsTyDell <milleb94@gmail.com>
2016-08-11 17:07:46 -0500

    add method to search or create by name and breed

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,6 +63,17 @@ class Dog
     end.first
   end
 
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
