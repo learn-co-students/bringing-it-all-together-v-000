7d7ad0976a4a03d1857da7ca2424cba78d95071e
Traci7822 <tracir@gmail.com>
2016-08-26 15:05:09 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,10 @@ require 'pry'
 class Dog
   attr_accessor :name, :breed, :id
 
-  def initialize(arguments)
-    @name = arguments[:name]
-    @breed = arguments[:breed]
-    @id = arguments[:id]
+  def initialize(id: nil, name:, breed:)
+    @name = name
+    @breed = breed
+    @id = id
   end
 
   def self.table_name
@@ -25,41 +25,32 @@ class Dog
   end
 
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE dogs
-      SQL
-
-      DB[:conn].execute(sql)
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
   end
 
   def save
-    if persisted? 
-      update 
+    if self.id
+      self.update
     else
       sql = <<-SQL
         INSERT INTO "#{self.class.table_name}" (name, breed)
         VALUES (?, ?)
-        SQL
+      SQL
+
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
     self
   end
 
-  def persisted?
-    !!self.id
-  end
-
   def update
-    sql = <<-SQL
-      UPDATE "#{self.class.table_name}" SET name = ?, breed = ?, WHERE id = ?
-    SQL
-
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-  def self.create(attributes)
-    new_dog = self.new(attributes)
+  def self.create(name:, breed:)
+    new_dog = self.new(name: name, breed: breed)
     new_dog.save
     new_dog
   end
@@ -75,22 +66,37 @@ class Dog
     self.new_from_db(row.first)
   end
 
-  def self.find_or_create_by(arguments)
+  def self.find_or_create_by(name:, breed:)
     if 
       sql =<<-SQL
         SELECT * 
         FROM "#{self.table_name}"
         WHERE name = ? 
+        AND breed = ?
       SQL
-
-      row = DB[:conn].execute(sql, arguments[:name])
-      row
-    else
-      self.create
+      found_dog = DB[:conn].execute(sql, name, breed)
+      if found_dog.empty?
+        dog = self.create(name: name, breed: breed)
+      else
+        dog = self.new(id: found_dog[0][0], name: found_dog[0][1], breed: found_dog[0][2])
+      end
+    dog
     end
   end
 
+  def self.find_by_name(name)
+    sql =<<-SQL
+    SELECT *
+    FROM "#{self.table_name}"
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    row = DB[:conn].execute(sql, name)
+    self.new_from_db(row.first)
+  end
+
   def self.new_from_db(row)
-   self.new(:name => row[1], :breed => row[2], :id => row[0])
+   self.new(name: row[1], breed: row[2], id: row[0])
   end
 end 
