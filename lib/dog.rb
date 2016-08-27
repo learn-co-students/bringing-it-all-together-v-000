68849d96b3a1bc7875693531993897ac8da0f25d
AliceKB <alicekristinabalbuena@gmail.com>
2016-08-27 14:17:31 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,85 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(name:,breed:,id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def self.new_from_db(row)
+    new_dog = self.new(name:row[1],breed:row[2],id:row[0])
+  end
+
+  def self.find_by_name(dog_name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+
+    DB[:conn].execute(sql,dog_name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_by_id(dog_id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+      LIMIT 1
+      SQL
+    DB[:conn].execute(sql,dog_id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_or_create_by(name:,breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(name:dog_data[1],breed:dog_data[2],id:dog_data[0])
+    else
+      dog = self.create(name:name,breed:breed)
+    end
+    dog
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name,breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql,self.name,self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql,self.name,self.breed,self.id)
+  end
+
+  def self.create(name:, breed:)
+    new_dog = Dog.new(name:name,breed:breed)
+    new_dog.save
+    new_dog
+  end
+end
