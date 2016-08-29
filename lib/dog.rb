2939e5c9d33451c5688baffed61851d31369c8bc
Perla Yates <pianistps@yahoo.com>
2016-08-29 23:17:37 +0000

    added create table method

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,22 @@
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(id=nil, name:, breed:)
+  def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
 end
