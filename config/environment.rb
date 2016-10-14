bb04f3f8426411cef25a03c017de526cbeee1bba
Emmanuel Owusu <e_owusu@live.com>
2016-10-14 00:36:34 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,14 @@ require 'sqlite3'
 require_relative '../lib/dog'
 
 DB = {:conn => SQLite3::Database.new("db/dogs.db")}
+DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+  SQL
+DB[:conn].execute(sql)
+DB[:conn].results_as_hash = true
