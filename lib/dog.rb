bcc4ddc3ab7e120a5210697345c65b05e7f80a0f
christinechoi <smi77y@gmail.com>
2016-11-02 21:03:08 -0400

    Six tests left

---
+++ lib/dog.rb
@@ -0,0 +1,101 @@
+
+class Dog
+  attr_accessor :name, :breed
+
+  attr_reader :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      color TEXT, 
+      breed TEXT, 
+      instagram TEXT)
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL 
+      DROP TABLE dogs
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.new_from_db(row)
+    new_dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+    new_dog
+  end
+
+  def self.find_by_name(doggy)
+    sql = <<-SQL
+      SELECT * FROM dogs 
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    result = DB[:conn].execute(sql, doggy).map do |row|
+      self.new_from_db(row)
+    end.first
+    
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE dogs 
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+
+  def save
+    if self.id
+      self.update   
+    else 
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
+  def self.create(name:, breed:)
+    doggy = Dog.new(name, breed)
+    doggy.save
+    doggy 
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM songs WHERE id = ?"
+
+    result = DB[:conn].execute(sql, id)[0]
+    dog = Dog.new(name: result[1], breed: result[2])
+    dog
+    
+  end
+
+  def find_or_create_by
+
+  end
+
+
+
+
+
+end
