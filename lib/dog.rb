a7811a5466381c0c65b8de67ce84e423a7955339
Senad Hadrovic <senadhadrovic@gmail.com>
2016-12-21 21:43:01 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,16 +52,17 @@ class Dog
 	  Dog.new(id: result[0], name: result[1], breed: result[2])
 	end
 
+
   def self.find_or_create_by(name:, breed:)
-   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
-   if !dog.empty?
-     dog_data = dog[0]
-     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
-   else
-     dog = self.create(name: name, breed: breed)
-   end
-    dog
-  end
+	  dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+          if !dog.empty?
+	     dog_data = dog[0]
+	     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+	  else
+	     dog = self.create(name: name, breed: breed)
+	  end
+	  dog
+	end
 
   def self.new_from_db(row)
     new_dog = self.new(id: row[0], name: row[1], breed: row[2])
