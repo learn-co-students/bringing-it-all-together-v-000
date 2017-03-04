db36cce54279adead2623c7844e04cc9ec0ddbbd
Juan Gongora <gongora.animations@gmail.com>
2017-03-04 21:54:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,10 +72,10 @@ class Dog
     end
       query
   end
-  #
-  # def self.new_from_db(row)
-  #   dog = self.new(id: row[0], name: row[1], breed: row[2])
-  # end
+  
+  def self.new_from_db(row)
+    dog = self.new(id: row[0], name: row[1], breed: row[2])
+  end
   #
   # def self.find_by_name(dog)
   #   sql = <<-SQL
