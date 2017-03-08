22778c17c88c1439052d7a1c77939d170c8f4f9c
bentonwong <bentonwong@gmail.com>
2017-03-08 12:32:06 -0800

    added .create_table

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,4 +10,16 @@ class Dog
     @breed = breed
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
 end
