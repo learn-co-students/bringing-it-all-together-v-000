9ab91da025a101df2fbdf34fd2c4cd03c5c6cecc
bentonwong <bentonwong@gmail.com>
2017-03-08 12:58:59 -0800

    added update, new_from_db, find_by_name

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,4 +31,34 @@ class Dog
     Dog.new(id:row[0],name:row[1],breed:row[2])
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+
+    new_dog_data = DB[:conn].execute(sql,name)[0]
+    self.new_from_db(new_dog_data)
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql,self.name,self.breed,self.id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?,?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
+
 end
