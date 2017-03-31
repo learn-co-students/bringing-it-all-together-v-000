18205f064701931e917535590f104c16b082f174
Trai Compton <tlcompton2546@outlook.com>
2017-03-31 16:58:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,10 +73,9 @@ class Dog
 
     dog = DB[:conn].execute(sql, name, breed)
     if !dog.empty?
-      info = dog.first
-      dog = self.new_from_db(info)
+      new_dog = self.new_from_db(dog.first)
     else
-      dog = self.create(name: name, breed: breed)
+      new_dog = self.create(name: name, breed: breed)
     end
   end
 
