c9a54d3723c03ed35145f63bede90fbbf85e8090
David Lewitan <dovidlwtn@gmail.com>
2017-04-09 23:55:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
-class Dog 
-  attr_accessor :name, :breed, :id 
+class Dog
+  attr_accessor :name, :breed, :id
 
   def initialize(attributes)
     @name = attributes[:name]
@@ -15,14 +15,17 @@ class Dog
     DB[:conn].execute("DROP TABLE IF EXISTS dogs;")
   end
 
-  def save 
-    sql = <<-SQL 
+  def save
+    sql = <<-SQL
       INSERT INTO dogs (name, breed) VALUES (?, ?);
     SQL
-
     DB[:conn].execute(sql, @name, @breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")[0][0]
     self
+  end
+
+  def update
+    DB[:conn].execute("UPDATE dogs SET name = ?, breed = ? WHERE id = ?;", @name, @breed, @id)
   end
 
   def self.create(attrs)
@@ -30,10 +33,18 @@ class Dog
   end
 
   def self.find_by_id(id)
-    sql = <<-SQL 
+    sql = <<-SQL
       SELECT * FROM dogs WHERE id = ?;
     SQL
     row = DB[:conn].execute(sql, id)[0]
+    row.length == 0 ? nil : self.reifyRow(row)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?;
+    SQL
+    row = DB[:conn].execute(sql, name)[0]
     row.length == 0 ? nil : self.reifyRow(row)
   end
 
@@ -47,12 +58,15 @@ class Dog
     sql = <<-SQL
       SELECT * FROM dogs WHERE name = ? AND breed = ?;
     SQL
-    found = DB[:conn].execute(sql, dog[:name], dog[:breed])[0]
-    if found.length == 0 
+    found = DB[:conn].execute(sql, dog[:name], dog[:breed])
+    if found.length == 0
       self.create(dog)
     else
-      self.reifyRow(found)
+      self.reifyRow(found[0])
     end
-  end 
-  
+  end
+
+  def self.new_from_db(row)
+    self.reifyRow(row)
+  end
 end
