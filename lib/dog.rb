278cbe6e9b8a80568cd48abc100801a02b27aa90
Sergio Mendoza <smendoza787@gmail.com>
2017-04-20 05:00:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,86 @@
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def self.create(name:, breed:)
+    new_dog = self.new(name: name, breed: breed)
+    new_dog.save
+    new_dog
+  end
+
+  def self.find_by_id(id_num)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
+
+    result = DB[:conn].execute(sql, id_num)[0]
+    self.new(id: result[0], name: result[1], breed: result[2])
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+
+    result = DB[:conn].execute(sql, name)[0]
+
+    self.new(id: result[0], name: result[1], breed: result[2])
+  end
+end
