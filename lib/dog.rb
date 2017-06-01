562dec2dd3f0784a34be815570f5231079cbb05e
Sam Zeidman <sam.zeidman@gmail.com>
2017-06-01 00:25:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog = self.new(row[0], row[1], row[2])
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
   end
 
   def self.find_by_name(name)
@@ -66,12 +66,43 @@ class Dog
         VALUES (?, ?)
       SQL
 
-      DB[:conn].execute(sql,self.name,self.breed)
+      DB[:conn].execute(sql, self.name, self.breed)
 
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-    self.find_by_name(self.name)
+    self
   end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
 
 
 
