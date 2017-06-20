508b8aad8dc1754ff744905589be7886d881ec93
benhayehudi <bengreenberg@gmail.com>
2017-06-20 15:05:08 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,104 @@
+class Dog
+
+attr_accessor :name, :breed, :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+      sql = <<-SQL
+        DROP TABLE dogs
+      SQL
+
+      DB[:conn].execute(sql)
+    end
+
+    def self.new_from_db(row)
+      new_dog = self.new(name: row[1], breed: row[2], id: row[0])
+      new_dog
+    end
+
+    def self.find_by_name(name)
+      sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE name = ?
+        LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
+    def update
+      sql = <<-SQL
+        UPDATE dogs
+        SET name = ?, breed = ?
+        WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
+    def save
+      if self.id
+        self.update
+      else
+        sql = <<-SQL
+          INSERT INTO dogs (name, breed)
+          VALUES (?, ?)
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.breed)
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+     end
+     self
+   end
+
+   def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs
+    WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.create(name:, breed:)
+    new_dog = self.new(name: name, breed: breed, id:nil)
+    new_dog.save
+    new_dog
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+    SELECT * FROM dogs
+    WHERE name = ? AND breed = ?
+    SQL
+    dog = DB[:conn].execute(sql, name, breed)
+    if !dog.empty?
+      dog_info = dog[0]
+      dog = self.new(name: dog_info[1], breed: dog_info[2], id: dog_info[0])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+ end
