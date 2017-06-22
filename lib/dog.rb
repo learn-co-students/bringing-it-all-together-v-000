c651204313e3f413db6c77464eb5049617d23d90
charlie lee <ctlee9096@gmail.com>
2017-06-22 18:40:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,6 +36,7 @@ class Dog
         SQL
         DB[:conn].execute(sql, self.name, self.breed)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        self
       end
   end
 
