3396bafdcfe9caa658b87c92fb54b9b5c3de1008
charlie lee <ctlee9096@gmail.com>
2017-06-22 21:06:32 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,4 +59,10 @@ class Dog
     end
   end
 
+  def self.find_by_id(id_num)
+    #binding.pry
+    sql = "Select id From dogs WHERE id = ?"
+    result = DB[:conn].execute(sql,id_num)[0]
+    Dog.new(id:result[0], name:result[1], breed:result[2])
+  end
 end
