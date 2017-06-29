df9faf7d9052e26ecff12b9b215ba54fc35c261d
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-29 23:19:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,20 +22,41 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def self::create(attributes)
-    new_dog = self.new(attributes)
+  def self::create(name:, breed:)
+    new_dog = self.new(name: name, breed: breed)
     new_dog.save
     new_dog
   end
 
-  def self::find_by_id
+  def self::find_by_id(id)
     sql = "SELECT * FROM dogs where id = ?"
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
   end
 
+  def self::find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[1], dog_data[2], dog_data[0])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
 
+  def self.new_from_db(row)
+    new_dog = self.new(name: row[1], breed: row[2], id: row[0])
+    new_dog
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
   def save
     sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
@@ -44,4 +65,8 @@ class Dog
     self
   end
 
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 end
