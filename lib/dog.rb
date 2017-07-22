00c5c1c9835c14e2783d04ba2807fe6ba69609c8
Seb Gertz <sebgertz@gmail.com>
2017-07-22 02:04:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,97 @@
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS
+    dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    new_dog = Dog.new(name: name, breed: breed, id: id)
+    new_dog
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map do |row|
+      Dog.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
+  def self.create(argument_hash)
+    dog = Dog.new(argument_hash)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id == ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT* FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
+    dog = DB[:conn].execute(sql, name, breed)
+
+    if !dog.empty?
+      dog_info = dog[0]
+      dog = Dog.new_from_db(dog_info)
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+end
