2222840650a3e6c6b682df473f815885fbf60671
jonathan <jonathanyi81@gmail.com>
2017-08-13 21:43:37 -0400

    initailze and create_table complete

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id: nil, name:, breed:)
+    @id, @name, @breed = id, name, breed
+  end # initalize
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end # create_table
+end
