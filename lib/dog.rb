c3c196354e05a29daf3da8f2222768418742d4c9
Rebecca Bratu <beckybratu@gmail.com>
2017-08-27 03:37:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,4 +16,53 @@ class Dog
      DB[:conn].execute(sql)
   end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+       )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+    end
+    self
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def self.create(hash)
+    dog = Dog.new(hash)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs
+    WHERE id = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, id)
+  end
+
 end
