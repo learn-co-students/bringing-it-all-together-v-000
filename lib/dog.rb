6be0a775b50dfc4dee6d4d576f743c627384d8a3
churchwerks <steven.lennis@icloud.com>
2017-09-26 09:04:14 -0500

    7 tests passing

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,24 @@
 require_relative "../config/environment.rb"
 
-class DOG
+class Dog
+
   attr_accessor :id, :name, :breed
+  @@all = []
 
   def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
-    @grade = breed
+    @breed = breed
+    @@all << self
+  end
+
+  def self.table_name
+    "#{self.to_s.downcase}s"
   end
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS dogs (
+    CREATE TABLE IF NOT EXISTS #{self.table_name} (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
@@ -24,5 +31,25 @@ class DOG
   def self.drop_table
     sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      update
+    else
+    sql = <<-SQL
+      INSERT INTO #{self.table_name} (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.table_name}")[0][0]
+    end
+    #binding.pry
+    self
+  end
+
+  def update()
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
