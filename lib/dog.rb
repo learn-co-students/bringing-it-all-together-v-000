2e3de171c8e385e9a5c0ca7405ab7badace715f8
GusVilla303 <gusvilla303@gmail.com>
2017-10-11 15:54:54 -0600

    Add self.create and got the spec to pass

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Dog
 
   attr_accessor :name, :breed
@@ -25,5 +27,26 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?);
+            SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+      Dog.new(id: @id, name: self.name, breed: self.breed)
+    end
+  end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
 end
