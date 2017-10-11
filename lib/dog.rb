87271b8ec1d2c60fd19c03d464dc07dc8efc0877
GusVilla303 <gusvilla303@gmail.com>
2017-10-11 17:45:24 -0600

    Refactor .find_or_create_by method to use .create instead of .new and .save

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id: nil, name: name, breed: breed)
+  def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
@@ -60,8 +60,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?;", name, breed)
     if dog.empty?
-      dog = self.new(name: name, breed: breed)
-      dog.save
+      dog = self.create(name: name, breed: breed)
     else
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
