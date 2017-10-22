a385a548af4078d5c8f440c1a37e3dc32f5cb45c
Denis Bondin <dsbondin@gmail.com>
2017-10-22 23:43:11 +0000

    working on update

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,6 +69,26 @@ class Dog
     end
   end
 
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1;
+    SQL
+
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
+  end
+
+  def update
+  end
+
+
 
 
 end
