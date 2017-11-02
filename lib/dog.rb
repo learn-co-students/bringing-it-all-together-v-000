b140b2dc4fd923ceb0ebbc206c3a2566af6d6db0
ff18qd <ff18qd@gmail.com>
2017-11-02 02:10:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,6 +37,21 @@ class Dog
     self
   end
 
+  def self.create(hash)
+    dog = Dog.new(hash)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+    SQL
+    row = DB[:conn].execute(sql, id)[0]
+    Dog.new_from_db(row)
+  end
+
   def self.new_from_db(row)
     hash = {}
     hash[:id] = row[0]
@@ -50,6 +65,23 @@ class Dog
     sql = "SELECT * FROM dogs WHERE name = ?"
     row = DB[:conn].execute(sql, name)[0]
     self.new_from_db(row)
+  end
+
+  def self.find_or_create_by(hash)
+    if self.find_by_name(hash[:name])
+      dog = self.find_by_name(hash[:name])
+      if dog.breed == hash[:breed]
+        dog
+      else self.create(hash)
+      end
+    else
+      self.create(hash)
+    end
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ? "
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
