cfdbcfa991a5566e81a695292842d240d1e2dadb
Sherry El Garhy <sherryptk@gmail.com>
2017-12-29 19:46:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+end
