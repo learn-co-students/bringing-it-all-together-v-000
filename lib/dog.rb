00890e8c0cea8e3a4efb83f2d28ae6207d21f87e
github username <github email address>
2018-01-22 11:56:50 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,81 @@
+class Dog
+
+  attr_accessor :name, :breed, :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT into dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs").flatten[0]
+    end
+    self
+  end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(number)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    dog = DB[:conn].execute(sql, number).flatten
+    instance = self.new(id: dog[0], name: dog[1], breed: dog[2])
+    instance
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
+    dog = self.new(id: row[0], name: row[1], breed: row[2])
+    dog
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    dog_info = DB[:conn].execute(sql, name).flatten
+    self.new(id: dog_info[0], name: dog_info[1], breed: dog_info[2])
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+end
