8091945bfaaa93a7750eb87055f4961c96ba2e5e
rh24 <ms.rebeccahong@gmail.com>
2018-01-23 13:17:37 -0500

    Fix self.create_table

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,14 +10,15 @@ class Dog
   end
 
   def self.create_table
-    # sql = <<-SQL
-    #   CREATE TABLE dogs
-    #     id INTEGER PRIMARY KEY,
-    #     name TEXT,
-    #     breed TEXT
-    #   SQL
-    #
-    # DB[:conn].execute(sql)
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
