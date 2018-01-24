777ae60c4a5d850a1aa83f6e01fb63942bc7d657
Chad Montoya <chadmontoya21@yahoo.com>
2018-01-24 01:15:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,8 +38,8 @@ class Dog
 
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-      self
     end
+    self
   end
 
 
@@ -80,6 +80,7 @@ class Dog
       SELECT *
       FROM dogs
       WHERE name = ?
+      LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map{|row| new_from_db(row)}.first
   end
