aeea0295b08ee702c77f4343c9285b1b1296dd78
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-01-31 22:57:13 -0500

    Rspeclemented Dog.create_table

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,14 @@ class Dog
     end
 
     def self.create_table
-        # does nothing
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            );
+        SQL
+
+        DB[:conn].execute(sql)
     end
 end
