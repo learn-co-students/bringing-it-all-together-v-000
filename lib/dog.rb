ec5fadbda218eb1a46a75307a8fb2524d5621c29
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-01-31 23:00:44 -0500

    Rspeclemented Dog.new_from_db(row)

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,4 +23,8 @@ class Dog
     def self.drop_table
         DB[:conn].execute('DROP TABLE dogs;')
     end
+
+    def self.new_from_db(row)
+        Dog.new(id: row[0], name: row[1], breed: row[2])
+    end
 end
