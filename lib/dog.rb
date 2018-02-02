4bff9be2f2f74a10c9dbfe583b5e15d81640c22d
funemployeddad <funemployeddad@gmail.com>
2018-02-02 16:04:29 -0700

    need to finish #find_or_create_by and #update

---
+++ lib/dog.rb
@@ -0,0 +1,90 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id = nil, hash)
+    @id = id
+    @name = hash[:name]
+    @breed = hash[:breed]
+  end
+
+  def self.create_table
+    sql= <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute('DROP TABLE IF EXISTS dogs')
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+        SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
+  def self.create(attributes)
+    dog = Dog.new(attributes)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+      sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE id = ?
+      SQL
+
+      result = DB[:conn].execute(sql, id)[0]
+
+      new_from_db(result)
+  end
+
+  def self.find_or_create_by(attributes)
+
+  end
+
+  def self.new_from_db(row)
+    hash = {:id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+
+    self.new(hash[:id], hash)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
+
+    result = DB[:conn].execute(sql, name)[0]
+
+    new_from_db(result)
+  end
+
+  def update
+    # sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    #   DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+end
