2f4426f9e79718b1cf03230cc57af983af1bd387
Araba Taylor <araba.taylor20@gmail.com>
2018-02-09 18:49:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,6 +53,16 @@ class Dog
     dog
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs where name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def self.find_by_id(id)
     sql = <<-SQL
       SELECT * FROM dogs WHERE id = ?
@@ -61,6 +71,22 @@ class Dog
     DB[:conn].execute(sql, id).map do |row|
         self.new_from_db(row)
       end.first
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      #binding.pry
+      dog = Dog.new(id:dog_data[0], name:dog_data[1], breed:dog_data[2])
+    else
+      song = self.create(name:name, breed:breed)
+    end
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
