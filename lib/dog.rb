ba897d03db04cbef81b8f9a7af3e1861647e36c7
github username <github email address>
2018-02-12 18:01:11 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,23 +65,23 @@ class Dog
     end
 
    #get an object by its NAME
-    def self.find_by_name(name)
-     sql = <<-SQL
-       SELECT * FROM #{self.table_name} WHERE name = ?;
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name} WHERE name = ?;
      SQL
      DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
-    end
+  end
 
-    def self.find_or_create_by(name:, breed:)
+  def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
-    if !dog.empty?
-      dog_data = dog[0]
-      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+  if !dog.empty?
+    dog_data = dog[0]
+    dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
     end
     dog
-  end 
+  end
 
 
 end
