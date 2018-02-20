10bb8a32d8ca56af30e2819a11dc2675f8323770
Katrina Brinson <kbrinso@gmail.com>
2018-02-20 19:40:55 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,4 +73,30 @@ class Dog
       return self.create({name: name, breed: breed})
     end
   end
+  
+  def self.new_from_db(row)
+    dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+    dog.save
+    dog
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+    SQL
+    
+    dog = DB[:conn].execute(sql, name)[0]
+    Dog.new(id: dog[0], name: dog[1], breed: dog[2])
+  end
+  
+  def update
+    # binding.pry
+    sql = <<-SQL
+      UPDATE dogs
+      SET id = ?, name = ?, breed = ?
+    SQL
+    
+    DB[:conn].execute(sql, self.id, self.name, self.breed)
+  end
 end
