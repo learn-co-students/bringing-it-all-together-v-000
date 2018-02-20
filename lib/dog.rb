785b3e8363876c461b74be7abbdc4c5fa296c834
Katrina Brinson <kbrinso@gmail.com>
2018-02-20 18:42:27 +0000

    starting find_by_id

---
+++ lib/dog.rb
@@ -0,0 +1,47 @@
+class Dog 
+  attr_accessor :id, :name, :breed
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    #newdog = 
+    self
+  end
+  
+  def self.create(hashattr)
+    dog = Dog.new(name: hashattr[:name], breed: hashattr[:breed])
+    dog.save
+    dog
+    # {:name=>"Ralph", :breed=>"lab"}
+  end
+end
