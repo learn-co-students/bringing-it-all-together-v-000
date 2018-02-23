db86d2c1409639eb917c46070e11a36bb543f0b7
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 21:07:52 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,41 @@
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name:, breed:)
+    self.name = name
+    self.id = id
+    self.breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute('DROP TABLE dogs')
+  end
+
+  def self.new_from_db(row)
+    id, name, breed = row
+    new(id: id, name: name, breed: breed)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE name = ?
+    SQL
+
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
+  end
+end
