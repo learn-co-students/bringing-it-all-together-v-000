ec087f29f2cefbffae09aea5fd081894b3502e1e
Chris Henry <cdhenry@gmail.com>
2018-03-08 21:25:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
-require 'pry'
+
 class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id = nil, dog_hash)
+  def initialize(id: nil, name:, breed:)
     @id = id
-    @name = dog_hash[:name]
-    @breed = dog_hash[:breed]
+    @name = name
+    @breed = breed
   end
 
   def self.create_table
@@ -46,34 +46,30 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-  def self.create(dog_hash)
-      new_dog = Dog.new(dog_hash)
-      new_dog.save
+  def self.create(name:, breed:)
+    new_dog = Dog.new(name: name, breed: breed)
+    new_dog.save
   end
 
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)[0]
-    dog_hash = {:name => result[1], :breed => result[2]}
-    Dog.new(result[0], dog_hash)
+    row = DB[:conn].execute(sql, id)[0]
+    self.new_from_db(row)
   end
 
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
-      dog_hash = {:name => dog[1], :breed => dog[2]}
-      dog = Dog.new(dog_data[0], dog_hash)
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      dog_hash = {:name => name, :breed => breed}
-      dog = self.create(dog_hash)
+      dog = self.create(name: name, breed: breed)
     end
     dog
   end
 
   def self.new_from_db(row)
-    dog_hash = {:name => row[1], :breed => row[2]}
-    new_dog = self.new(row[0], dog_hash)
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
   end
 
   def self.find_by_name(name)
@@ -83,8 +79,7 @@ class Dog
     SQL
 
     row = DB[:conn].execute(sql, name).flatten
-    dog_hash = {:name => row[1], :breed => row[2]}
-    Dog.new(row[0], dog_hash)
+    self.new_from_db(row)
   end
 
 end
