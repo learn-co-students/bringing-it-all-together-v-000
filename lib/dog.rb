c66e569ca080342e847060bc508f2edda9181a14
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-09 21:14:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,55 @@
+require_relative "../config/environment.rb"
+
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = db[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+ #  def save
+ #   if self.id
+ #     self.update
+ #   else
+ #     sql = <<-SQL
+ #       INSERT INTO dogs (name, breed)
+ #       VALUES (?, ?)
+ #     SQL
+ #     DB[:conn].execute(sql, self.name, self.breed)
+ #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+ #   end
+ #   self
+ # end
+
+end
