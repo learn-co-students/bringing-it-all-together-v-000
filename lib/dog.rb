7a73203250d4a61a6bb026df5e90d267e9aaccee
Nadia Lee <32117376+missnadia@users.noreply.github.com>
2018-03-21 18:11:42 -0400

    Update dog.rb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,8 @@
 require_relative "../config/environment.rb"
 
 class Dog
-  attr_accessor :name, :breed, :id
+  attr_accessor :name, :breed
+  attr_reader :id
 
   def initialize(id: nil, name:, breed:)
     @id = id
