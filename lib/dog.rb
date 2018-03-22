183ede7778a1f6e207883f84090ed5009914bcf5
Ariana Berger <ariana.berger@gmail.com>
2018-03-22 23:20:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Dog
 
   attr_accessor :name, :breed
@@ -49,13 +51,29 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    dog_data = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)[0]
-    if !dog_data.empty?
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0] #set this here and not in the line above, because if it is empty, it will cause an error!
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
     end
     dog
+  end
+
+  def self.new_from_db(row)
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    dog_data = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(dog_data)
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
