0a0a67ce7171b95390c3b640c3e6b58c17bc2f1c
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-16 09:50:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,13 +30,17 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed) VALUES (?, ?)
-      SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) VALUES (?, ?)
+        SQL
 
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    self
+        DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        self
+    end 
   end
 
   def self.new_from_db(row) #create an instance from a database record as an array
@@ -97,7 +101,7 @@ class Dog
       WHERE id = ?
       SQL
 
-    DB[:conn].execute(sql, self.name, self.breed, self.id) 
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
