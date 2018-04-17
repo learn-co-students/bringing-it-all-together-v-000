52728cec38c5c32386e49f8e926dbbaf63b608e0
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-17 18:55:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,18 +14,14 @@ class Dog
         id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
-      )
+      );
     SQL
 
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE dogs
-    SQL
-
-    DB[:conn].execute(sql)
+    DB[:conn].execute("DROP TABLE dogs")
   end
 
   def save
@@ -44,50 +40,29 @@ class Dog
   end
 
   def update
-    sql = <<-SQL
-      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
-    SQL
-
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-  def self.new_from_db(row)
-    self.new(id: row[0], name: row[1], breed: row[2])
-  end
-
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM dogs
-      WHERE dogs.name = ?
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+  def self.create(attributes)
+    dog = Dog.new(attributes)
+    dog.save
+    dog
   end
 
   def self.find_by_id(id)
-    sql = <<-SQL
-      SELECT *
-      FROM dogs
-      WHERE id = ?
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, id).map do |row|
+    sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1"
+    result = DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
   end
 
-  def self.create(attributes)
-    new_dog = self.new(attributes)
-    new_dog.save
+  def self.new_from_db(row)
+    dog = self.new(id: row[0], name: row[1], breed: row[2])
   end
 
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE dogs.name = ? AND dogs.breed = ?", name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
@@ -97,5 +72,10 @@ class Dog
     dog
   end
 
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM dogs WHERE name = ? LIMIT 1", name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 end
