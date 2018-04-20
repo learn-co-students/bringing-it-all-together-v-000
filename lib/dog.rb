c09c6f04cf8e57a589caed8d09cfa0fa1cca98e0
Dee Race <deerace12@gmail.com>
2018-04-20 18:28:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,16 +35,15 @@ class Dog
       SQL
 
       DB[:conn].execute(sql, self.name, self.breed)
-      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
       end
       self
     end
 
-  def self.create(dog)
-    new_dog = Dog.new(dog)
-    id, name, breed = dog[:id], dog[:name], dog[:breed]
-    new_dog.save
-    new_dog
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
   end
 
   def self.find_by_id(id)
@@ -59,9 +58,9 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    find_dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    if !find_dog.empty?
-      dog_data = find_dog[0]
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
@@ -71,8 +70,9 @@ class Dog
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
+
     DB[:conn].execute(sql, name).map do |row|
-    self.new_from_db(row)
+      self.new_from_db(row)
     end.first
   end
 
