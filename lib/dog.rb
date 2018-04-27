3fbf262df6c02ecc5b8313b56db368735012189b
Vanessa Toro <vanetoro84@gmail.com>
2018-04-27 18:42:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,8 @@ class Dog
     end
 
     def self.create_table
+      DB[:conn].execute('DROP TABLE IF EXISTS dogs')
+
       sql = <<-SQL
         CREATE TABLE dogs (
           id INTEGER PRIMARY KEY,
@@ -16,6 +18,56 @@ class Dog
         )
       SQL
 
-      db[:conn].execute(sql)
+      DB[:conn].execute(sql)
     end
+
+    def save
+      if !self.id
+        sql = <<-SQL
+        INSERT INTO dogs (name, breed) VALUES (? , ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs ")[0][0]
+      end
+      self
+    end
+
+    def self.drop_table
+        DB[:conn].execute('DROP TABLE IF EXISTS dogs')
+    end
+
+    def self.create(attributes)
+      dog = Dog.new(name: attributes[:name], breed: attributes[:breed])
+      dog.save
+    end
+
+    def self.find_by_id(id)
+      sql = <<-SQL
+        SELECT * FROM dogs WHERE id = ?
+      SQL
+
+      row = DB[:conn].execute(sql, id).flatten
+      dog = Dog.new(name: row[1], breed: row[2], id: row[0])
+    end
+
+    def self.find_or_create_by(attributes)
+      dog = DB[:conn].execute('SELECT * FROM dogs WHERE name = ? AND breed= ?', attributes[:name], attributes[:breed]).flatten
+        if !dog.empty?
+          find_by_id(dog[0])
+        else
+          create(attributes)
+        end
+      end
+
+      def self.new_from_db(row)
+          dog = self.new(name:row[1], breed:row[2], id:row[0])
+      end
+
+      def self.find_by_name(name)
+        sql = <<-SQL
+          SELECT * FROM dogs WHERE name = ?
+        SQL
+
+          DB[:conn].execute(sql, name)
+      end
 end
