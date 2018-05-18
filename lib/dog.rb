27174c43326aa435918db6a1dc0d20d7588f2789
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-18 01:42:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,9 +5,30 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id = nil, name:, breed:)
+  def initialize(id = nil, hash)
       @id = id
-      @name = name
-      @breed = breed
+      @name = hash[:name]
+      @breed =hash[:breed]
   end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def update
+
 end
