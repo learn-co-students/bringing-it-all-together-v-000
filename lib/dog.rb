165cf588b80cb4f0147b300c58bbbff626cbe78f
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-18 01:21:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id = nil, name, breed)
+  def initialize(id = nil, name:, breed:)
       @id = id
       @name = name
       @breed = breed
