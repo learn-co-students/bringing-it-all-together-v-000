682b4cd27960c5e858b19096ff457d0b3dfdc51f
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-19 16:29:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,10 +5,10 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id = nil, hash)
+  def initialize(id: nil, name:, breed:)
       @id = id
-      @name = hash[:name]
-      @breed =hash[:breed]
+      @name = name
+      @breed = breed
   end
 
   def self.create_table
@@ -30,5 +30,36 @@ class Dog
   end
 
   def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("Select last_insert_rowid() FROM dogs")[0][0]
+    end
+     self
+  end
+
+  def self.create(hash)
+    new_dog = Dog.new(hash)
+    new_dog.save
+    new_dog
+  end
+
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    dog = DB[:conn].execute(sql,id)[0]
+  #  find_dog = [name:dog[1],breed:dog[2],id:dog[0]]
+    Dog.new(id: dog[0], name: dog[1], breed: dog[2])
+  end
 
 end
