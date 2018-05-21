8c7b987d3518ee8c423dd59d166e0773c33f616c
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-05-21 11:47:54 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,9 +27,9 @@ class Dog
   end
 
   def save
-    # if self.id
-    #   self.update
-    # else
+    if self.id
+      self.update
+    else
     sql = <<-SQL
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)
@@ -38,7 +38,7 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
 
-    # end
+    end
     self
   end
 
