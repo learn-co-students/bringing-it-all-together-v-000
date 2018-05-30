5ed96c7dfa0a38800061670fee2800725dd1c4f9
Ron Sala <ron.sala@gmail.com>
2018-05-30 05:35:55 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,12 +27,12 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
-    self.new(id, name, breed)
-  end
+  # def self.new_from_db(row)
+  #   id = row[0]
+  #   name = row[1]
+  #   breed = row[2]
+  #   self.new(id, name, breed)
+  # end
 
   def self.find_by_name(name)
     # find the student in the database given a name
@@ -94,8 +94,8 @@ class Dog
       dog
   end
 
-  def new_from_db(row)
-    dog = Dog.new(row[0], row[1], row [2])
+  def self.new_from_db(row)
+    dog = Dog.new(id: row[0], name: row[1], breed: row [2])
     dog
   end
 end
