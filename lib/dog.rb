af43e0af2fd89c9218c81b8881924bbfa8117e19
Ron Sala <ron.sala@gmail.com>
2018-05-30 04:21:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,20 +70,9 @@ class Dog
     self
   end
 
-  def self.create(name:, breed:)
-    # dog = Dog.new(name, breed) # wrong number of arguments (given 2, expected 0)
-    # dog = Dog.new # missing keywords: name, breed
-    # dog = Dog.new(name, breed) # wrong number of arguments (given 2, expected 0)
-    # dog = Dog.new(name:, breed:) #unexpected ',' (SyntaxError) dog = Dog.new(name:, breed:) # wrong number of ar...
-    dog = Dog.new(name:, breed:) # wrong number of ar...
-    binding.pry
-    # dog.name = name
-    # dog.breed = breed
-    # dog.name = @name # missing keywords: name, breed
-    # dog.breed = @breed
-    # dog.name = name:
-    # dog.breed = breed: #syntax error, unexpected ':', expecting keyword_end
+  # def self.create(name:, breed:)
+  def self.create(hash)
+    dog = Dog.new(hash)
     dog.save
-    dog
   end
 end
