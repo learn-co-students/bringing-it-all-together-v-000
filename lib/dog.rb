4be9723bada1e13054b82b1c93519a94b0325222
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-31 23:06:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(id:nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def create_table
+
+  end
+
+end
