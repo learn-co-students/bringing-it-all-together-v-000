ec9b515c802a236f49c11e7adb19aaa75a1b743d
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-04 17:34:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,35 +19,44 @@ def self.create_table
        DB[:conn].execute(sql)
      end
 
-     def self.drop_table
-            sql =  <<-SQL
-              DROP TABLE dogs
-                SQL
-            DB[:conn].execute(sql)
-          end
+def self.drop_table
+  sql =  <<-SQL
+  DROP TABLE dogs
+  SQL
 
-          def update
-                sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
-                DB[:conn].execute(sql, self.name, self.breed, self.id)
-                 end
+  DB[:conn].execute(sql)
+end
 
-               def save
-                 if self.id
-                    self.update
-                else
-                   sql = <<-SQL
-                     INSERT INTO dogs (name, breed)
-                     VALUES (?, ?)
-                   SQL
-                   DB[:conn].execute(sql, self.name, self.breed)
-                   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-                 end
-                 end
+def update
+  sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.breed, self.id)
+end
 
-                 def self.create(name, breed)
-                        dog = Dog.new(name, breed)
-                        dog.save
-                        dog
-                    end
+def save
+  if self.id
+    self.update
+  else
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+end
+
+def self.create(name, breed)
+  dog = Dog.new(name, breed)
+  dog.save
+  dog
+end
+
+def self.new_from_db(row)
+  id = row[0]
+  name =  row[1]
+  grade = row[2]
+  self.new(id, name, grade)
+end
 
 end
