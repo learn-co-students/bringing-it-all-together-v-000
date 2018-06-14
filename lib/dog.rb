d53700370dad24c732b46f6a358878b77094f0ff
sgibson47 <sgibson47@gmail.com>
2018-06-14 17:09:30 -0500

    add #update

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,4 +25,13 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def update
+    sql = <<-SQL
+    UPDATE dogs
+    SET name =?, breed =?
+    WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
 end
