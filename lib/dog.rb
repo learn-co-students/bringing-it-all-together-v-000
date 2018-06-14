7341f1510786c26ece6b1757df997ca132dfa75c
sgibson47 <sgibson47@gmail.com>
2018-06-14 17:30:28 -0500

    add #find_by_name

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,5 +78,18 @@ class Dog
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * 
+    FROM dogs
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
