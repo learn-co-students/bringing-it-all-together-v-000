b6a75d7e99c3994bdcb60a2bf826fc060406ed4b
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-02 19:38:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
+
 class Dog
-  attr_accessor :breed, :name, :id
+  attr_accessor :breed, :name
+  attr_reader :id
   
   def initialize(name:, breed:, id: nil)
     self.name = name 
     self.breed = breed
-    self.id = id
+    @id = id
     self
   end
   
@@ -29,16 +31,54 @@ class Dog
   end
   
   def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) 
+      VALUES (?, ?)
+    SQL
+  
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
+    self
+  end
+  
+  def self.create(name:, breed:)
+    new(name: name, breed: breed).tap { |pup| pup.save }
+  end
+  
+  def self.find_by_id(dogs_id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+    SQL
     
+    DB[:conn].execute(sql, dogs_id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
-  def new_from_db
+  def self.find_or_create_by(name: name, breed: breed)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ? AND breed = ?
+    SQL
+    
+    dog_found = DB[:conn].execute(sql, name, breed)
+    
+    unless dog_found.empty?
+      find_by_id(dog_found[0][0])
+    else
+      create(name: name, breed: breed)
+    end
   end
   
-  def find_by_name
+  def self.new_from_db(row)
+    new(id: row[0], name: row[1], breed: row[2])
   end
   
-  def update
+  def self.find_by_name
+    
   end
   
 end
