58e4f0f306f76d6cc66c92df5ffbd446785021af
William J Teskey <willjteskey@gmail.com>
2018-07-10 19:00:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,82 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id: nil, name:, breed: )
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      Dog.new(id: @id, name: self.name, breed: self.breed)
+    end
+  end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+
+    result = DB[:conn].execute(sql, id)[0]
+    self.new(id: result[0],name: result[1], breed: result[2])
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?;", name, breed)
+    if dog.empty?
+      dog = self.create(name: name, breed: breed)
+    else
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
+    dog = self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    row = DB[:conn].execute(sql, name).flatten
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+end
