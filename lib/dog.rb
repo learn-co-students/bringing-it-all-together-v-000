394c9422f56ed4f134eeab39117ab1b42b65673c
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-20 02:26:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,13 +39,25 @@ class Dog
     end
   end
 
-  def self.create(new_hash:)
-    doggy = self.new(new_hash[1],new_hash[2],new_hash[0])
-    doggy.save
-    doggy
+  def self.new_from_db(row)
+    new_dog = self.new(name:row[1], breed:row[2], id:row[0])
+    new_dog
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
-
+  def update
+    sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
 end
