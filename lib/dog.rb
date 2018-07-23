4be6f237af287947177418ee213250227ff67a76
Nathan  Visak <nathan.visak@gmail.com>
2018-07-23 21:45:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize (name:, breed:, id:nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)"
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    #returns an instance of the dog class, probably wants a Ruby object
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
+end
