93fd641361a9b54e28e86c5cf8dde652b15eac41
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-06 21:35:21 +0000

    6 errors to go

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,16 @@
 class Dog
-  ATTRIBUTES = {
-    :id => "INTEGER PRIMARY KEY",
-    :name => "TEXT",
-    :breed => "TEXT"
-  }
+  attr_accessor :id , :name, :breed
 
-  ATTRIBUTES.keys.each do |attribute_name|
-    attr_accessor attribute_name
-  end
+
+  # ATTRIBUTES = {
+  #   :id => "INTEGER PRIMARY KEY",
+  #   :name => "TEXT",
+  #   :breed => "TEXT"
+  # }
+  #
+  # ATTRIBUTES.keys.each do |attribute_name|
+  #   attr_accessor attribute_name
+  # end
 
   def initialize(id: nil, name:, breed:)
     @id = id
@@ -25,6 +28,50 @@ class Dog
         SQL
     DB[:conn].execute(sql)
   end
+
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE IF EXISTS dogs;
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+
+  def self.create(id: nil, name:, breed:)
+    dog = self.new(id: id, name: name, breed: breed)
+    dog.save
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE dogs.id = ?
+    SQL
+
+    dog = DB[:conn].execute(sql, id)[0]
+    Dog.new(id:dog[0],name:dog[1],breed:dog[2])
+  end
+
+
+  def update
+
+  end
+
 
 
 
