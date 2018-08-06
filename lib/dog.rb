63184847779b79eb122d71d7f0465d2a727fa10d
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-06 22:21:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,6 @@
 class Dog
   attr_accessor :id , :name, :breed
 
-
-  # ATTRIBUTES = {
-  #   :id => "INTEGER PRIMARY KEY",
-  #   :name => "TEXT",
-  #   :breed => "TEXT"
-  # }
-  #
-  # ATTRIBUTES.keys.each do |attribute_name|
-  #   attr_accessor attribute_name
-  # end
-
   def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
@@ -35,8 +24,6 @@ class Dog
         SQL
     DB[:conn].execute(sql)
   end
-
-
 
   def save
     if self.id
@@ -67,12 +54,33 @@ class Dog
     Dog.new(id:dog[0],name:dog[1],breed:dog[2])
   end
 
-
-  def update
-
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
   end
 
+  def self.new_from_db(dog_data)
+    dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+  end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE dogs.name = ?
+    SQL
 
+    dog = DB[:conn].execute(sql, name)[0]
+    Dog.new(id:dog[0],name:dog[1],breed:dog[2])
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
 end
