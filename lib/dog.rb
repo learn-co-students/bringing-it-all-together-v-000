9486fb6cef98dc5c8ff729d5d52d9e9f40d5b2dc
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-08-12 03:09:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,68 @@
+require_relative "../config/environment.rb"
+
+class Dog
+
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    self.create(name:, breed:)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+  end
+
+  # def self.find_by_id(id:)
+  #   sql = <<-SQL
+  #   SELECT *
+  #   FROM dogs
+  #   WHERE id = ?
+  #   LIMIT 1
+  #   SQL
+  #
+  #   DB[:conn].execute(sql, id).map do |row|
+  #     self.new_from_db(row)
+  #   end.first
+  # end
+
+end
