08932f0b16d6d144a713250817c45a0aee43b74e
Emily J Cain <emilycain@ymail.com>
2018-08-15 11:22:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,16 @@ class Dog
   end 
   
   def self.create_table
-    
+    sql = "CREATE TABLE dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT)"
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table 
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
   end 
   
 end 
