5633db1d05e4aab8fc6ae43a5dba86656f9e2f2b
Emily J Cain <emilycain@ymail.com>
2018-08-15 11:18:21 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  
+  attr_accessor :id, :name, :breed
+  
+  def initialize(id: nil, name:, breed:)
+    @id = id 
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def self.create_table
+    
+  end 
+  
+end 
+    
