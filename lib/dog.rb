695c6a5b8d44bd91ec779b0f02788a20d9898e33
Emily J Cain <emilycain@ymail.com>
2018-08-15 11:42:10 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,7 @@ class Dog
   end 
   
   def self.create_table
-    sql = "CREATE TABLE dogs (
+    sql = "CREATE TABLE IF NOT EXISTS dogs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT)"
@@ -20,6 +20,32 @@ class Dog
     sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
   end 
+  
+  def self.new_from_db(row)
+    new_dog = Dog.new(row)
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
+    new_dog
+  end 
+  
+  
+  
+  def update 
+    
+  end 
+    
+  
+  def save
+    new_dog = Dog.new(name, breed)
+    sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.breed)
+    
+    @id = DB[:conn].execute("SELECT last_insert_rowid()
+    FROM dogs")[0][0]
+  end 
+  
+  
   
 end 
     
