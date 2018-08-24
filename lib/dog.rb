e264c3cd15e997a9636c8105b1b4a18d0262fcc6
Sushi Chanrai <sushi@pathworks.co>
2018-08-24 10:13:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,14 +49,14 @@ attr_reader :id
 
 
   def self.find_or_create_by(name:, breed:)
-    song = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, album)
-    if !song.empty?
-      song_data = song[0]
-      song = Song.new(song_data[0], song_data[1], song_data[2])
+    doggy = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !doggy.empty?
+      dog_data = doggy[0]
+      dog = Dog.new(id:dog_data[0],name:dog_data[1],breed:dog_data[2])
     else
-      song = self.create(name: name, album: album)
+      dog = self.create(name: name, breed: breed)
     end
-    song
+    dog
   end
 
 
