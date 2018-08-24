69bc26a00a49b28457143b09900649072aa0d80f
Sushi Chanrai <sushi@pathworks.co>
2018-08-24 10:32:28 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,5 +59,21 @@ attr_reader :id
     dog
   end
 
+  def self.new_from_db(row)
+    dog = Dog.new(id:row[0],name:row[1],breed:row[2])
+  end
+
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end  
+
 
 end
