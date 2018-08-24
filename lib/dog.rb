c61768bab329b3e988d4aa12a32fa95a89eb7a62
Sushi Chanrai <sushi@pathworks.co>
2018-08-24 10:04:41 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,4 +26,38 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+  def self.create(hash)
+    doggy = Dog.new(hash)
+    doggy.save
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(id:result[0],name:result[1],breed:result[2])
+  end
+
+
+  def self.find_or_create_by(name:, breed:)
+    song = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end
+
+
 end
