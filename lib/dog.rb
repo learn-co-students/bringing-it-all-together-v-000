f61ecc5fb2c50148a8d583ceaa165986d8370c07
Ayana Bando <ayanaflax@yahoo.com>
2018-09-07 20:44:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,46 @@
+require "pry"
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+     id INTEGER PRIMARY KEY,
+     name TEXT,
+     album TEXT
+     )
+     SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+  sql = "DROP TABLE IF EXISTS dogs"
+  DB[:conn].execute(sql)
+end
+
+  def save
+    if self.id
+      self.update
+        else
+          sql = <<-SQL
+            INSERT INTO dogs(name, breed)
+            VALUES (?, ?)
+          SQL
+
+          DB[:conn].execute(sql, self.name, self.breed)
+          @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      end
+  end
+
+  def self.create
+    dog = Dog.new(name, breed)
+    dog.save
+    dog
+  end
+end
