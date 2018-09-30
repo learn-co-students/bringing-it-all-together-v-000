79bde0beab5616c126543d6c11d77471b4b28ec5
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-09-30 05:14:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,10 +7,8 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id=nil, name:, breed:)
-    @id = id
-    @name = name
-    @breed = breed
+  def initialize(id: nil, name:, breed:)
+    @id, @name, @breed = id, name, breed
   end
 
   def self.create_table
