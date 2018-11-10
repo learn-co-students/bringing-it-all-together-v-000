28713974e9c6afe59159fcb5cddce2b0670cbbad
Samantha brake <sammy.brake@gmail.com>
2018-11-10 05:34:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,40 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+  sql = <<-SQL
+    DROP TABLE dogs
+  SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+   sql = <<-SQL
+   INSERT INTO dogs (name, breed)
+   VALUES (?, ?)
+   SQL
+   DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+self
+  end
+
+end
