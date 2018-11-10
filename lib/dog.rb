f34408f4fb1e6020c4f72a8f53db57ad4addb35a
Samantha brake <sammy.brake@gmail.com>
2018-11-10 15:29:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,5 +47,13 @@ class Dog
     dog
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+     SELECT * FROM dogs WHERE id = ?
+    SQL
+    dog_info = DB[:conn].execute(sql, id)[0]
+    self.new(name: dog_info[1],breed: dog_info[2], id: dog_info[0])
+  end
+
 
 end
