3a49389a0d4572698d165e4a702338078beb406c
Greg Morgan <gregm9999@gmail.com>
2018-11-22 18:11:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,13 @@
+require_relative "../config/environment.rb"
 require "pry"
+
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-    self.id || nil
+  def initialize(id: nil, name:, breed:)
+    @id=id
+    @name=name
+    @breed=breed
   end
 
   def self.create_table
@@ -19,22 +22,26 @@ class Dog
   end
 
   def self.drop_table
-    sql = "DROP TABLE dogs"
+    sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed)
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      end
     self
   end
 
-  def self.create(attributes)
-    dog=Dog.new(attributes)
+  def self.create(name:, breed:)
+    dog=Dog.new(name: name, breed: breed)
     dog.save
     dog
   end
@@ -51,23 +58,23 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     sql = <<-SQL
       SELECT * FROM dogs
-      WHERE name = ? AND breed = ?
+      WHERE name = ? AND breed = ? LIMIT 1
     SQL
-    dog = DB[:conn].execute(sql, name, breed).first
-    if dog
-      new_dog = self.new_from_db(dog)
+    dog = DB[:conn].execute(sql, name, breed)
+    if !dog.empty?
+      dog_data=dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      new_dog = self.create(:name=> name, :breed=>breed)
+      dog = self.create(:name=> name, :breed=>breed)
     end
+    dog
   end
 
   def self.new_from_db(row)
-    attributes = {
-      :id=> row[0],
-      :name => row[1],
-      :breed => row[2]
-    }
-    self.new(attributes)
+    id = row[0]
+    name= row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end
 
   def self.find_by_name(name)
