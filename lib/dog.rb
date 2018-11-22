9bfaea0a53543859a653915ba7b7a9fb8ff22f81
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-22 21:09:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,85 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  DB = {:conn => SQLite3::Database.new("db/dogs.db")}
+  
+  def initialize(id: nil, name:, breed:)
+    local_variables.each do |k|
+      v = eval(k.to_s)
+     instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end 
+  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        breed TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs;
+      SQL
+      
+    DB[:conn].execute(sql)
+  end
+  
+  def self.new_from_DB(row)
+    Dog.new(row[0], row[1], row[2])
+  end 
+  
+  def self.find_by_name(name) 
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+  
+  def update
+    sql = "UPDATE songs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+  def save 
+    if self.id 
+      self.update 
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?);
+        SQL
+      
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
+  
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+  
+  def self.find_by_id(id) 
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+  
+  def self.find_or_create_by(name:, breed:)
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
+    result = DB[:conn].execute(sql, name, breed)
+    if result.empty?
+      dog = self.create(name: name, breed: breed)
+    else
+      row = result[0]
+      dog = Dog.new(row[0], row[1], row[2])
+    end
+    dog
+  end
+end 
--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -7,13 +7,7 @@ describe "Dog" do
 
   before(:each) do
     DB[:conn].execute("DROP TABLE IF EXISTS dogs")
-    sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS dogs (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        breed TEXT
-        )
-    SQL
+    
     DB[:conn].execute(sql)
   end
 
