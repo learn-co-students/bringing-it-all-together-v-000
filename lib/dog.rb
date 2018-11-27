42ece7387f74e74f495bf1bf8b41514dab488082
Rafael Lopez <lpz.raf@gmail.com>
2018-11-27 03:58:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog 
+  attr_accessor :id, :name, :breed
+  
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table()
+    sql =  <<-SQL 
+    CREATE TABLE dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT, 
+    breed TEXT
+    );
+    SQL
+    DB[:conn].execute(sql) 
+  end
+  
+  def self.drop_table
+   sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+  
+end
