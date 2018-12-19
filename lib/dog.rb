408a707d06da4b5536a53ab76f9d98647c7452a7
taisaoko <ttd3calvin@gmail.com>
2018-12-19 23:32:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,64 @@
+class Dog
+  attr_accessor :name, :breed, :id
+  
+  def initialize (id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+     sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+      SQL
+  
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
+  
+  def self.create(name, breed)
+    dog = Dog.new(name, breed)
+    dog.save
+    dog
+  end
+
+  def self.new_from_db(row)
+      new_dog = self.new(row[0], row[1], row[2])  # self.new is the same as running Dog.new
+      new_dog  # return the newly created instance
+      # create a new Dog object given a row from the database
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs. WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+
+end
