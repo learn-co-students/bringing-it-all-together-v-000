66d43cb7c760b738e119b6bae5abf106712c348b
github username <github email address>
2018-12-28 18:47:31 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,12 +48,6 @@ class Dog
     self
   end
 
-  def self.find_by_name(name)
-    sql = "SELECT * FROM dogs WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Dog.new(result[0], result[1], result[2])
-  end
-
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
@@ -68,10 +62,26 @@ class Dog
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
-    new_dog = Dog.new(result[0], result[1], result[2])
+    new_dog = Dog.new(id: result[0], name: result[1], breed: result[2])
     new_dog
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(id: result[0], name: result[1], breed: result[2])
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
 
 
 end
