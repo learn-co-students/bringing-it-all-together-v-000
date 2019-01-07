e5386e69a9f2bb9581366938a622893eb024d81c
Nick Romans <nicholas.romans@gmail.com>
2019-01-07 20:27:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Dog
 
   attr_accessor :name, :breed
@@ -25,6 +26,57 @@ class Dog
      DROP TABLE dogs
      SQL
      DB[:conn].execute(sql)
+   end
+
+   def self.new_from_db(row)
+     id = row[0]
+     name = row[1]
+     breed = row[2]
+     new_dog = self.new(id: id, name: name, breed: breed)
+   end
+
+   def self.find_by_name(name)
+     sql = <<-SQL
+     SELECT *
+     FROM dogs
+     WHERE name = ?
+     LIMIT 1
+     SQL
+
+     DB[:conn].execute(sql, name).map do |row|
+       self.new_from_db(row)
+     end.first
+   end
+
+   def update
+     sql = <<-SQL
+     UPDATE dogs
+     SET name = ?, breed = ?
+     WHERE id = ?
+     SQL
+
+     DB[:conn].execute(sql, self.name, self.breed, self.id)
+   end
+
+   def save
+     if self.id
+       self.update
+     else
+       sql = <<-SQL
+       INSERT INTO dogs (name, breed)
+       VALUES (?, ?)
+       SQL
+       DB[:conn].execute(sql, self.name, self.breed)
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+   end
+
+   def self.create(hash)
+     dog_name = hash[:name]
+     dog_breed = hash[:breed]
+     new_dog = self.new(name: dog_name, breed: dog_breed)
+     new_dog.save
    end
 
 end
