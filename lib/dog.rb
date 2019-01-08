ca87aa1685e5c45cc08261a3c883575a425a9881
Ashley Wong <ashwong04@gmail.com>
2019-01-08 14:28:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,9 +9,30 @@ class Dog
   end
   
   def self.create_table
-  
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT)
+      SQL
+    DB[:conn].execute(sql)
   end
   
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+  end
+  
+  def save
+    
+  end
+  
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+  
+  def self.find_by_name
+    
+  end
   
   
   
