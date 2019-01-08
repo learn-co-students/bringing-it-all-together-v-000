4f3072d64aabe80821aac5c2363d22b73f576d1b
Ashley Wong <ashwong04@gmail.com>
2019-01-08 14:30:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,8 +30,8 @@ class Dog
     self.new(id: row[0], name: row[1], breed: row[2])
   end
   
-  def self.find_by_name
-    
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)
   end
   
   
