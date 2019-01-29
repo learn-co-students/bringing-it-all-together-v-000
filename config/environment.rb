af0f72d73b6db5e96f6a3506a90357b1e1a0ad3c
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-29 00:10:14 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
 require_relative '../lib/dog'
 
+#=> responsible for setting up and maintaining connection to application's database
 DB = {:conn => SQLite3::Database.new("db/dogs.db")}
