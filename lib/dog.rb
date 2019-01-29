81aa36f514dd9781568d57cf8a2daac34bcaf4bb
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-29 20:07:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,7 @@
 require 'pry'
 
 class Dog
-  attr_accessor :name, :breed
-  attr_reader :id
+  attr_accessor :name, :breed, :id
   
   @@all = []
   
@@ -10,11 +9,12 @@ class Dog
     @name = name
     @breed = breed 
     @id = id
+    @@all << self
   end
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE dogs (
+      CREATE TABLE IF NOT EXISTS dogs (
         id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
@@ -25,33 +25,60 @@ class Dog
   
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE dogs;
+      DROP TABLE IF EXISTS dogs;
     SQL
     DB[:conn].execute(sql)
   end
   
   def self.new_from_db(row)
-    binding.pry
-    new_dog = self.new(name = row[1], breed = row[2], id = row[0])
+    new_dog = self.new(name: row[1], breed: row[2])
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
+    new_dog #=> returns an instance of a dog
   end
   
-  def save
+  def self.find_by_name(name)
     sql = <<-SQL
-      INSERT INTO dogs (name, breed)
-      VALUES (?, ?)
+      SELECT * FROM dogs
+      WHERE name = ?
     SQL
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+    result = DB[:conn].execute(sql, name)[0]
+    binding.pry
+    Dog.new(id: result[0], name: result[1], breed: result[2])
   end
   
-  def self.create(attributes_hash)
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+    SQL
   
+    result = DB[:conn].execute(sql, id)[0]
   end
+  
+  # def self.create(name:, breed:)
+  #   binding.pry
+  # end
   
   def update
     sql = <<-SQL
       UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
     DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
   end
 end
