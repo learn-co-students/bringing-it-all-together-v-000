feb840f01a89bd5db7c6b998e6d6b644eabd5028
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-29 01:27:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,12 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
   
-  def initialize(id: nil, name:, breed:)
-    @id = id
+  @@all = []
+  
+  def initialize(name:, breed:, id: nil)
     @name = name
-    @breed = breed
+    @breed = breed 
+    @id = id
   end
   
   def self.create_table
@@ -28,7 +30,28 @@ class Dog
     DB[:conn].execute(sql)
   end
   
+  def self.new_from_db(row)
+    binding.pry
+    new_dog = self.new(name = row[1], breed = row[2], id = row[0])
+  end
+  
   def save
-    
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
+  
+  def self.create(attributes_hash)
+  
+  end
+  
+  def update
+    sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
