bbdb3e5582be9f0ed70ff79cca0c8d1bde1cb043
tgray017 <tgray017@gmail.com>
2019-02-05 13:01:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,4 +58,22 @@ class Dog
     end
   end
   
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?;
+    SQL
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
+  end
+  
+  def update
+    
+  end
+  
 end
