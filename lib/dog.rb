61b3bb807de027b8d0dbecabc4b2fc75fd726ac9
tgray017 <tgray017@gmail.com>
2019-02-05 13:04:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,7 +73,12 @@ class Dog
   end
   
   def update
-    
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
   
 end
