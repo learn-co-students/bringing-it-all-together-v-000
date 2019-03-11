661748a411e3f4e766a2c7361ec45081ee72597c
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-11 20:56:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,11 +40,6 @@ attr_reader :id
     dog
   end
 
-  #def self.new_from_db(row)
-    #id, name, breed = row[0], row[1], row[2]
-    #self.new(id, name, grade)
-  #end
-
   def self.find_by_id(id)
    sql = "SELECT * FROM dogs WHERE id = ?"
    result = DB[:conn].execute(sql, id)[0]
@@ -54,11 +49,27 @@ attr_reader :id
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
-      dog_data = dog[0]
-      dog = self.new(dog_data[0], dog_data[1], dog_data[2])
+      result = dog[0]
+      dog = Dog.new(id: result[0], name: result[1], breed: result[2])
     else
       dog = self.create(name: name, breed: breed)
     end
-    self
+    dog
+  end
+
+  def self.new_from_db(row)
+    id, name, breed = row[0], row[1], row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    row = DB[:conn].execute(sql, name)
+    self.new(id: row[0][0], name: row[0][1], breed: row[0][2])
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
