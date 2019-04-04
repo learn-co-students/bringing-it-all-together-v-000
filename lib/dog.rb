288503e071a751472d76c43d414c30c7ec7c7499
Reno Sorensen <renojames19@gmail.com>
2019-04-04 00:29:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,4 +58,18 @@ class Dog
     dog
   end
 
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    elsif dog.include?(name)
+      dog_data = dog[0]
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
 end
