062c0687c45a5c18d004cc1a39675167dea2f774
Karly Mareka <karly.mareka@gmail.com>
2019-04-06 03:42:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,8 +52,39 @@ attr_accessor :name, :breed, :id
     dog = Dog.new(dog_info_hash)
   end
   
-  def self.find_or_create_by(:name, :breed)
-    dog = DB[:conn].execute
-    
+  def self.find_or_create_by(dog_info)
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
+    dog = DB[:conn].execute(sql, dog_info[:name], dog_info[:breed])[0]
+    if dog != nil
+      dog_info[:id] = dog[0]
+      dog = Dog.new(dog_info)
+    else
+      dog = Dog.create(dog_info)
+    end
+    return dog 
+  end
+  
+  def self.new_from_db(dog)
+    dog_info = {:id => dog[0], :name => dog[1], :breed => dog[2]}
+    new_dog = Dog.new(dog_info) 
+  end
+  
+  def self.find_by_name(name)
+         sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+     
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? 
+          WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end 
 end 
