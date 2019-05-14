f3d5b64aaa52fbfa2592ec190508dcf1025afcf3
Paul Fell <swellpf@gmail.com>
2019-05-14 19:05:55 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,11 +27,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog = self.new
-    new_dog.id = row[0]
-    new_dog.name = row[1]
-    new_dog.breed = row[2]
-    new_dog
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end
 
 
