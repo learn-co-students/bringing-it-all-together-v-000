25fab365bcec8e4d47826078b32cacc54d82825d
Paul Fell <swellpf@gmail.com>
2019-05-15 08:07:58 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,6 +43,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
+    self
   end
 
   def update
@@ -59,5 +60,36 @@ class Dog
       self.new_from_db(row)
     end.first
   end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ? AND breed = ? LIMIT 1
+    SQL
+    dog = DB[:conn].execute(sql, name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
 
 end
