68f151b568e5337b216264d40ad3d35bc82ff6fa
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 16:58:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,26 +37,72 @@ class Dog
     DB[:conn].execute(sql)
   end 
 
-  # def self.new_from_db(row)
-  
-  # end
-
-  # def self.find_by_name(name)
-  #   SELECT * FROM dog WHERE name = ?
-  # end 
-
-  # def update
-    
-  # end 
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end 
   
   def save
-    SELECT * FROM dog WHERE id = ?
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if 
-      empty array
-      INSERT
+      dog.empty?
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     else
-      found to exist
-      UPDATE      
+      update      
     end
+    self
+  end
+  
+  def self.create(dog_attributes)
+    new_dog = Dog.new(dog_attributes)
+    new_dog.save
+  end 
+  
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL 
+      SELECT * 
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1 
+    SQL
+
+     DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+ def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+   def self.find_by_name(name)
+    sql = <<-SQL 
+      SELECT * 
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1 
+    SQL
+
+     DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
