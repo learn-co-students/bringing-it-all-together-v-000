445c65c732e40321cd1799ad6231113a6404b8d6
Amar Chatman <amarjerome@gmail.com>
2019-09-22 23:39:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,9 +8,30 @@ def initialize(id: nil, name:, breed:)
 end
 
 def self.create_table
-<<-SQL
-  CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);
-  SQL
+  sql =  <<-SQL
+       CREATE TABLE IF NOT EXISTS dogs (
+         id INTEGER PRIMARY KEY,
+         name TEXT,
+         breed TEXT
+         )
+         SQL
+     DB[:conn].execute(sql)
 end
+
+
+def self.drop_table
+  sql =  <<-SQL
+     DROP TABLE IF  EXISTS dogs
+       SQL
+   DB[:conn].execute(sql)
+end
+
+def save
+  sql =  <<-SQL
+     DROP TABLE IF  EXISTS dogs
+       SQL
+   DB[:conn].execute(sql)
+end
+
 
 end
