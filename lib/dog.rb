8616b42989fea2a9de5f988468507cd220d498d9
Olivia Kesler <oakesler@protonmail.ch>
2019-12-12 04:53:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,4 +62,21 @@ class Dog
     new_dog
   end
   
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(id: result[0], name: result[1], breed: result[2])
+  end
+  
+   def self.find_or_create_by(name: , breed: )
+     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+  
 end
