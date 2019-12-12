adea605b135098a90f377693cd2accc43e90a890
Olivia Kesler <oakesler@protonmail.ch>
2019-12-12 04:55:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,4 +79,12 @@ class Dog
     dog
   end
   
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(id: result[0], name: result[1], breed: result[2])
+  end
+  
+  
+  
 end
