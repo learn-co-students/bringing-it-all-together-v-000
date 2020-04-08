a321d87b28e50611a0518bae9f8520f7c387f762
Luis Herrera <herrera.lrh@gmail.com>
2020-04-08 19:54:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 class Dog
-  attr_accessor :name, :breed, :id
+  attr_accessor :name, :breed
+  attr_reader :id
 
   def initialize(id: nil, name:, breed:)
     @id = id
@@ -24,21 +25,6 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def self.new_from_db(row)
-    Dog.new(id: row[0], name: row[1], breed: row[2]).tap {|dog|}
-  end
-
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM dogs
-      WHERE name = ?
-      LIMIT 1;
-    SQL
-
-    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
-  end
-
   def update
     sql = <<-SQL
       UPDATE dogs
@@ -55,11 +41,11 @@ class Dog
     else
       sql = <<-SQL
         INSERT INTO dogs (name, breed)
-        VALUES (?,?);
+        VALUES (?,?)
       SQL
 
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
     end
     self
   end
@@ -76,11 +62,15 @@ class Dog
       LIMIT 1;
     SQL
 
-    DB[:conn].execute(sql, id).map { |row| self.new_from_db(row)  }.first
+    DB[:conn].execute(sql, id).map { |row| self.new_from_db(row) }.first
+  end
+
+  def self.new_from_db(row)
+    Dog.new(id: row[0], name: row[1], breed: row[2]).tap {|dog| dog}
   end
 
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?;", name, breed)
     if !dog.empty?
       dog_attr = dog[0]
       dog = Dog.new(id: dog_attr[0], name: dog_attr[1], breed: dog_attr[2])
@@ -90,4 +80,15 @@ class Dog
     dog
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1;
+    SQL
+
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
+
+  end
 end
