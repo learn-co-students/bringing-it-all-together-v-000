a1c272e96cc0f85e89c5eff4d58df0a5343c1483
Paris Dolfman <parisdolfman@gmail.com>
2020-07-06 17:15:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,5 +18,19 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+  end
+
+  def save
+     sql = <<-SQL
+       INSERT INTO dogs (name, breed)
+       VALUES (?, ?)
+     SQL
+
+     DB[:conn].execute(sql, self.name, self.breed)
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+     self
+  end
 
 end
