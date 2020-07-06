685bea8d65754a08a7e9fedb5e7b5b3ccc50b9b5
Paris Dolfman <parisdolfman@gmail.com>
2020-07-06 17:35:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,7 +34,16 @@ class Dog
   end
 
   def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end
 
 end
