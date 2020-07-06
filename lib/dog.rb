fd7fae595878a4c274401b7cf676dbfc4fc898ae
Paris Dolfman <parisdolfman@gmail.com>
2020-07-06 17:57:22 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,4 +56,15 @@ class Dog
     end.first
   end
 
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+      if !dog.empty?
+        dog_data = dog[0]
+        dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+      else
+        dog = self.create(name: name, breed: breed)
+  end
+  dog
+end
+
 end
