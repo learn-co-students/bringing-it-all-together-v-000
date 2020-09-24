3874205e9a652411d7119d737aeb93e1b43b64f0
Paris Dolfman <parisdolfman@gmail.com>
2020-09-24 19:09:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,4 +68,14 @@ class Dog
   dog
 end
 
+def self.find_by_name(name)
+  sql = <<-SQL
+  SELECT * FROM dogs WHERE name = ? LIMIT 1
+  SQL
+
+  DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+  end.first
+end
+
 end
