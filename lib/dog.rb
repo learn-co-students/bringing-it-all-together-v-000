eafdb0aa380ad3a54644054a157df193887c5b53
github username <github email address>
2021-03-04 15:53:47 -0500

    add save method

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,58 @@
 require 'pry'
-require_relative "..config/environment.rb"
+require_relative "../config/environment.rb"
 
 class Dog
-    attr_accessor :name, :breed
+  attr_accessor :name, :breed, :id
 
-    def initialize(id:nil, name:, breed:)
-      @id = id
-      @name = name
-      @breed = breed
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      breed TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
     end
 
-    def self.create_table
-        sql = <<-SQL
-          CREATE TABLE IF NOT EXISTS dogs (
-              id INTEGER PRIMARY KEY, 
-              name TEXT, 
-              breed TEXT
-          )
-          SQL
-        DB[:conn].execute(sql)
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs;"
+    DB[:conn].execute(sql)
+  end
+
+  def self.update 
+    sql = "UPDATE dogs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL 
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end 
 
 
+    
 
 
 end
