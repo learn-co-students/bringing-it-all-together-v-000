1ca494fafe767357561c7b3717badfc9ab66d615
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-04 21:43:28 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,7 +10,7 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS dog (
+    CREATE TABLE IF NOT EXISTS dogs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
