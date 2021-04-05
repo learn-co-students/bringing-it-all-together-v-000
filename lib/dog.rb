063c467592a020f3910d250bc5ae13f7e985246f
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-04 22:08:58 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,14 +10,22 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      breed TEXT
-    )
-    SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
 
-    DB[:conn].execute(sql)
+      DB[:conn].execute(sql)
   end
     
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs 
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+  
 end
